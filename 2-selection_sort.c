26bcb23683fad5cc0f81c7ab15d6c6397fc6b40a
DaboRestrepo <daborestrepo@gmail.com>
2022-02-11 21:35:56 -0500

    Sort with selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * selection_sort - Sort the array with selection sort method.
+ * @array: array to be sorted.
+ * @size: array size.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min_i = 0;
+	int temp = 0;
+
+	for (; i < size - 1; i++)
+	{
+		min_i = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_i])
+				min_i = j;
+		}
+		temp = array[min_i];
+		array[min_i] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +21,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 #endif
