aa6d96d35b928fdebe1742cd1990dcae9765d741
DaboRestrepo <daborestrepo@gmail.com>
2022-02-14 09:55:46 -0500

    Add if size > 0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,17 +12,20 @@ void selection_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (; i < size - 1; i++)
+	if (size > 0)
 	{
-		min_i = i;
-		for (j = i + 1; j < size; j++)
+		for (; i < size - 1; i++)
 		{
-			if (array[j] < array[min_i])
-				min_i = j;
+			min_i = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[min_i])
+					min_i = j;
+			}
+			temp = array[min_i];
+			array[min_i] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
-		temp = array[min_i];
-		array[min_i] = array[i];
-		array[i] = temp;
-		print_array(array, size);
 	}
 }
